1da430c5447c002fd94efd3d6625aeb3aeb7d699
koji <kk2796@nyu.edu>
2026-02-25 04:08:22 -0500

    address feedback

--- src/labels/inputDeleteLabel.ts
+++ src/labels/inputDeleteLabel.ts
@@ -3,5 +3,8 @@ import { deleteLabel } from '../constant.js'
 
 export const getTargetLabel = async (): Promise<readonly string[]> => {
   const name = await askText(deleteLabel.message)
+  if (!name.trim()) {
+    throw new Error("Label name can't be empty")
+  }
   return [name]
 }
