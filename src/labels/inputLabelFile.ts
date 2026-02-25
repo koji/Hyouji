6fcf6f8ad66e05d733e2cea31500ab71d0b34539
koji <kk2796@nyu.edu>
2026-02-25 04:04:11 -0500

    fix empty string case

--- src/labels/inputLabelFile.ts
+++ src/labels/inputLabelFile.ts
@@ -2,5 +2,11 @@ import { askText } from '../cli/promptClient.js'
 import { labelFilePath } from '../constant.js'
 
 export const getLabelFilePath = async (): Promise<string> => {
-  return askText(labelFilePath.message)
+  while (true) {
+    const filePath = (await askText(labelFilePath.message)).trim()
+    if (filePath.length > 0) {
+      return filePath
+    }
+    console.log('File path cannot be empty. Please try again.')
+  }
 }
